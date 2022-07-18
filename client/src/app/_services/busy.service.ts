dbf2389fc6bcef838841830bd2042832c209d8ce
Peter Essien <agyekumessien@yahoo.com>
2022-07-17 20:11:59 -0400

    fixed Loading

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -8,11 +8,12 @@ export class BusyService {
   busyRequestCount = 0;
 
   constructor(private spinnerService: NgxSpinnerService) { }
-
+//ball-spin-clockwise
+//line-scale-party
   busy() {
     this.busyRequestCount++;
     this.spinnerService.show(undefined, {
-      type: 'line-scale-party',
+      type: 'ball-spin-clockwise',
       bdColor: 'rgba(255,255,255,0)',
       color: '#333333'
     });
